ae6ee4b404e5e97c587984e2c8f75940da7f8a6e
raphaeltaurai <shawntaurai@gmail.com>
2025-12-05 23:45:45 +0200

    pdf template

---
+++ pdf-templates/config.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Gravity PDF Custom Template Config
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+$template_config = [
+	'name'        => 'Event Ticket Template',
+	'description' => 'PDF ticket including event details, logo, and QR code.',
+	'version'     => '1.0',
+	'author'      => 'Raphael Shawn Taurai',
+	'group'       => 'Custom Templates',
+	'template'    => 'pdf-templates.html',
+	'css'         => 'pdf-templates.css',
+];
+
+
+//so this is just a test to push the files to GitHub
+//This is another commit
+
+
